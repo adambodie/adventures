7a936aa87434336ffd65d5d9e00d4a6cdf28d75e
Adam Bodie <aduo3000@yahoo.com>
2017-09-16 12:05:17 -0700

    First commit

---
+++ src/components/Application.js
@@ -0,0 +1,23 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Application extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Application;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+var UglifyJsPlugin = require("uglify-js-plugin");
+var webpack = require('webpack');
+var webpackConfig = {
+  entry: './src/app.js',
+
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel-loader']
+      },
+      {
+        test: /\.s?css$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
+
+module.exports = webpackConfig;
