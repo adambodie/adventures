ed4bf4d747a12ec6505ba475146ff6392c152b82
adambodie <aduo3000@yahoo.com>
2017-09-17 12:01:44 -0700

    Added basic mobile/desktop layout

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,8 +13,31 @@ class Application extends React.Component {
   }
 
   render() {
+	let width = 4;
+	let height = 3;
     return (
       <div>
+		<div className="header">
+			<h1>Adam's Adventures</h1>
+		</div>
+		<div className="content">
+			<div className="sidebar">
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>
+			<img src="http://via.placeholder.com/200x200"/>						
+			</div>
+			<div className="carousel">
+			<img src={`http://via.placeholder.com/${width * 150}x${height *150}`}/>
+			</div>
+		</div>
+		<div className="footer">
+			<h3>Adam Bodie &copy; 2017</h3>
+		</div>
       </div>
     );
   }
