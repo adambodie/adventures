64b098d75bee447c33807bcb813fae23585ea2bf
Adam Bodie <aduo3000@yahoo.com>
2017-09-23 15:13:24 -0700

    Turned ItemList to a class

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Item = props => (
-        <img src={`./src/images/adventure${props.index}.jpg`} alt={props.title}/>
+        <img src={`./src/images/adventure${props.index}.jpg`} alt={props.title} onClick={props.handleClick}/>
 );
 
 export default Item;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ScrollArea from 'react-scrollbar';
 import Item from './Item';
 
-const ItemList = props => {
-  let adventures = props.data;
-  console.log(adventures);
-
-  let itemList = adventures.map((x, index) =>
-    <Item title={x.title} key={x.index} index={index + 1}/>
-  )
-  let scrollbarStyles = {borderRadius: 5};
-  return(
-        <ScrollArea
-          className="area"
-          contentClassName="content"
-          verticalScrollbarStyle={scrollbarStyles}
-          verticalContainerStyle={scrollbarStyles}
-          horizontalScrollbarStyle={scrollbarStyles}
-          horizontalContainerStyle={scrollbarStyles}
-          smoothScrolling= {true}
-          minScrollSize={40}
-          >
-      {itemList}
-      </ScrollArea>
-  );
+export default class ItemList extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+      this.onHandleClick = this.onHandleClick.this(bind);
+    }
+    onHandleClick() {
+      console.log("testing");
+    }
+    render(){
+    let adventures = this.props.data;
+    let itemList = adventures.map((x, index) =>
+      <Item title={x.title} key={x.index} index={index + 1} handleClick={x.onHandleClick}/>
+    )
+    let scrollbarStyles = {borderRadius: 5};
+    return(
+          <ScrollArea
+            className="area"
+            contentClassName="content"
+            verticalScrollbarStyle={scrollbarStyles}
+            verticalContainerStyle={scrollbarStyles}
+            horizontalScrollbarStyle={scrollbarStyles}
+            horizontalContainerStyle={scrollbarStyles}
+            smoothScrolling= {true}
+            minScrollSize={40}
+            >
+        {itemList}
+        </ScrollArea>
+    );
+  }
 }
-
-export default ItemList;
