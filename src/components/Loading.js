f71d396826da775fcc718b261e1eb17d876f0c0e
Adam Bodie <adam.bodie@yahoo.com>
2017-10-11 17:12:13 -0700

    Converted frown image to gif

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -13,7 +13,7 @@ export default class Loading extends Component {
 			{ isFailed ? (
 				<div className="loading">
 					<h1>File Not Found</h1>
-					<img src="./src/images/frown.jpg" alt = "Frown" />					
+					<img src="./src/images/frown.gif" alt = "Frown" />					
 				</div> ) : (
 				<div className="loading">
 					<h1>Loading...</h1>
