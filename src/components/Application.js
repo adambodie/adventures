b1d62afc2f03d2f4c1ae4e65c99ca5acb71c62df
Adam Bodie <adam.bodie@yahoo.com>
2017-10-22 16:08:58 -0700

    Added ComingSoon component

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,7 +8,7 @@ import {
 import Content from './Content';
 import Sidebar from './Sidebar';
 import Home from './Home';
-
+import ComingSoon from './ComingSoon';
 
 export default class Application extends Component {
   constructor(props) {
@@ -19,7 +19,9 @@ export default class Application extends Component {
 			{backgroundImage: 'hawaii'},
 			{backgroundImage: 'europe'},
 			{backgroundImage: 'seattle'},
-			{backgroundImage: 'cross-country'}
+			{backgroundImage: 'cross-country'},
+			{backgroundImage: 'minnesota'},
+			{backgroundImage: 'green-bay'}
           ]
     };
   }
@@ -39,11 +41,13 @@ export default class Application extends Component {
 			</div>
 			<div className="carousel">
 				<Route exact path = "/" render={() => <Home />}/>
-				<Route path="/vegas" render={() => <Content title='High School Graduation Trip to Las Vegas' id='72157687042225613' page= '37' background='vegas' color='white' />}/>
-				<Route path="/hawaii" render={() => <Content title='Mahalo Fora Maui' id='72157686468511520' page= '76' background='hawaii' color='gold' />  }/>
+				<Route path="/vegas" render={() => <Content title='High School Graduation Trip to Las Vegas' id='72157687042225613' page= '37' background='vegas' color='lightblue' />}/>
+				<Route path="/hawaii" render={() => <Content title='Mahalo Fora Maui' id='72157686468511520' page= '76' background='hawaii' color='#FF9933' />  }/>
 				<Route path="/europe" render={() => <Content title='My Trip to Europe' id='72157686893038650' page= '138' background='europe' color='gold' />  }/>
 				<Route path="/seattle" render={() => <Content title='Birthday Trip to Seattle' id='72157687246355471' page= '32' background='seattle' color='black' /> }/>
 				<Route path="/cross-country" render={() => <Content title='Cross Country Trip to Minnesota' id='72157688200510913' page= '66' background='cross-country' color='white' /> }/>
+				<Route path="/minnesota" render={() => <ComingSoon /> }/>
+				<Route path="/green-bay" render={() => <ComingSoon /> }/>
 			</div>
 		</div>
 		</Router>
