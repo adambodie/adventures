33577818ac936473e9312c3b8e03d2685759ae87
adambodie <adam.m.bodie@gmail.com>
2019-03-14 17:23:00 -0700

    Renamed BannerList test to Store

--- src/tests/store.test.js
+++ src/tests/store.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import Application from '../components/Application.js';
-
 import * as actions from '../store/actions/index';
 import * as reducers from '../store/reducers/index';
 import * as types from '../store/constants/ActionTypes';
 
-describe('actions', () => {
+describe('store', () => {
 	it('should create an action to increment count', () => {
 		const number = 0
 		const expectedAction = {
@@ -24,13 +21,15 @@ describe('actions', () => {
 		expect(actions.decrement(number)).toEqual(expectedAction)
 	});
 	it('should handle INCREMENT', () => {
-		expect(reducers.counter(0, {
-				type: types.INCREMENT
-		})).toEqual(1)
+		expect(reducers.counter(0, { type: types.INCREMENT })).toEqual(1)
 	});
 	it('should handle DECREMENT', () => {
-		expect(reducers.counter(1, {
-				type: types.DECREMENT,
-		})).toEqual(0)
+		expect(reducers.counter(1, { type: types.DECREMENT })).toEqual(0)
+	});	
+	it('should handle LOADED', () => {
+		expect(reducers.isLoaded(false, { type: types.LOADED })).toEqual(true)
+	});
+	it('should handle FAILED', () => {
+		expect(reducers.isFailed(false, { type: types.FAILED })).toEqual(true)
 	});	
 })
